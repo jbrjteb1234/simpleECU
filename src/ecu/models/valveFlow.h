1202f336a88d5c0d519962c0b791e986cd5cd5bb
jbrjteb1234 <james@bright.me.uk>
2025-07-23 23:50:52 +0100

    Comments

--- src/ecu/models/valveFlow.h
+++ src/ecu/models/valveFlow.h
@@ -1,5 +1,5 @@
 typedef struct {
-    float valveDia_mm;          // physical diameter
+    float valveDia_mm;          
     float Cd_lowLift;           // Cd at 0.05 * dia
     float Cd_fullLift;          // Cd at ≥0.25 * dia
 } valveFlow;  
